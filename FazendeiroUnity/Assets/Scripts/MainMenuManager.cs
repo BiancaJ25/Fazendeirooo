ab17ac4fd2d0a607c0103b7b06de637f93d9332a
BiancaJ25 <20233024265@estudantes.ifpr.edu.br>
2026-04-14 10:50:49 -0300

    splash art

--- FazendeiroUnity/Assets/Scripts/MainMenuManager.cs
+++ FazendeiroUnity/Assets/Scripts/MainMenuManager.cs
@@ -7,21 +7,10 @@ public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private string nomeLVLjogo;
     [SerializeField] private GameObject painelMainMenu;
-    [SerializeField] private GameObject painelOpcoes;
 
     public void Jogar()
     {
         SceneManager.LoadScene(nomeLVLjogo);
-    }
-    public void AbrirOpcoes()
-    {
-        painelMainMenu.SetActive(false);
-        painelOpcoes.SetActive(true);
-    }
-    public void FecharOpcoes()
-    {
-        painelOpcoes.SetActive(false);
-        painelMainMenu.SetActive(true);
     }
     public void SairJogo()
     {
